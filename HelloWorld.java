234889291c34b9ce039c85b362f2761521572d25
tufpie <tufpie@users.noreply.github.com>
2016-08-05 13:37:51 +1200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
   public static void main (String [] args) 
   {
-     System.out.println("Hello TUFPIE - this looks like it's working.. :-))")
+     System.out.println("Hello TUFPIE - this looks like it's working..")
      
   }
   
